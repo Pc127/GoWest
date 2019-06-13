46de7ddba1f4104377b9f2e5a773dbc2fba615ab
Peter Xie <515971172@qq.com>
2019-06-13 13:40:39 +0800

    try to merge 1-3

---
+++ Assets/Script/CommonManager/Delay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Delay: MonoBehaviour
+
+{
+    public static IEnumerator DelayToInvokeDo(Action action, float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        action();
+    }
+
+}
---
+++ Assets/Script/NpcManageer/Boss.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GamePersist.GetInstance().subtitle = "啊，官逼民反啊。";
+        StartCoroutine(Delay.DelayToInvokeDo(() =>
+        {
+            if (!GamePersist.GetInstance().hero.isHide)
+            {
+                GamePersist.GetInstance().subtitle = "你死了";
+                StartCoroutine(EndGame());
+            }
+        }, 1f));
+       
+    }
+
+    IEnumerator EndGame()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Application.LoadLevel(0);
+
+    }
+}
