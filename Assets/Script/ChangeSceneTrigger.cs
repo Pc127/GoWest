bffa6070093ad4c3948c5d6d44b0c3833f6b870e
Peter Xie <515971172@qq.com>
2019-06-13 17:45:40 +0800

    add 1-3's entry

---
+++ Assets/Script/ChangeSceneTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneTrigger : MonoBehaviour
+{
+    public int sceneIndex;
+
+    public float delay;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(delay);
+        // 加载场景
+        Application.LoadLevel(sceneIndex);
+    }
+}
